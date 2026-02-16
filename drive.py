ada2895aec105b5b1de9cbb494c399afbbaa3c59
Aakash Kumar <aakashinthesky@gmail.com>
2026-02-16 09:55:17 -0800

    Update motor configuration mapping in drive.py

--- drive.py
+++ drive.py
@@ -194,12 +194,12 @@ def main():
 
     commander = MotorController()
     motor_config = {
-        0: (1, 0), 
-        1: (2, 3), 
-        2: (4, 5), 
-        3: (7, 6), 
-        4: (9, 8), 
-        5: (10, 11), 
+        0: (0, 6), 
+        1: (1, 7), 
+        2: (2, 8), 
+        3: (3, 9), 
+        4: (4, 10), 
+        5: (5, 11), 
     }
     motors = []
     for motor_id in range(6):
